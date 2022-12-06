2034a21e1f353d37e376718fad575a51f9fb0d4b
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2022-12-06 15:21:00 +0100

    Initial commit

---
+++ build.sh
@@ -0,0 +1,3 @@
+mpicc -std=c++17 -o main main.c -I /home/ivo/opt/spack/opt/spack/linux-ubuntu20.04-skylake/gcc-9.4.0/hdf5-1.12.2-2xgyynh7r6eqfsmdunkilg7c3olpt2k5/include -L /home/ivo/opt/spack/opt/spack/linux-ubuntu20.04-skylake/gcc-9.4.0/hdf5-1.12.2-2xgyynh7r6eqfsmdunkilg7c3olpt2k5/lib/ -lhdf5 -lhdf5_hl
+mpirun -np 4 ./main
+h5dump SDS.h5
